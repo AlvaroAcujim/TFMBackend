d62445c06290c5c65346d9a406427bbdad10ab51
Álvaro <alvarorodrigoaj@gmail.com>
2025-06-18 16:10:42 +0200

    fix: changed cors url origin

--- src/app.js
+++ src/app.js
@@ -13,14 +13,13 @@ const fileRoutes = require('./routes/fileRoutes');
 const app = express();
 const axios = require('axios');
 const config = require('../config.js');
-console.log("API_KEY cargada:", process.env.API_KEY);
 app.use(express.json());
 
 app.use(cookieParser());
 //https://tfm-frontend-flame.vercel.app
 app.use(cors(
   {
-    origin: ['http://localhost:5173', 'https://tfm-frontend-lt1bys3as-alvaros-projects-0bcb003c.vercel.app'], 
+    origin: ['http://localhost:5173', 'https://tfm-frontend-flame.vercel.app'], 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']              
@@ -41,34 +40,6 @@ const apiLimiter = rateLimit({
     max: 300,
     message: 'Demasiadas peticiones desde esta IP',
   });
-async function testHuggingFaceAPI() {
- try {
-    const response = await axios.post(
-    'https://api.together.xyz/v1/chat/completions',
-    {
-      model: 'mistralai/Mixtral-8x7B-Instruct-v0.1', // o llama-3-8b
-      messages: [{ role: 'user', content: prompt }],
-      max_tokens: 1024,
-      temperature: 0.7,
-      stop: ['```', '\n\n'] // Opcional
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.TOGETHER_API_KEY}`,
-        'Content-Type': 'application/json'
-      },
-      timeout: 30000
-    }
-  );
-
-  console.log(response.data.choices[0].message.content)
-  } catch (error) {
-    console.error('Error:', error.response?.data);
-  }
-}
-testHuggingFaceAPI()
-  .then(() => console.log('Prueba completada'))
-  .catch(() => console.log('Prueba fallida'));
 
 app.use('/api', apiLimiter);
 app.use('/api/users', userRoutes);
